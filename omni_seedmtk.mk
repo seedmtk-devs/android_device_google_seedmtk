d9c3ecf6e43e054ebf2fd7041bec5220bdf83984
Harsh Shandilya <MSF-Jarvis@users.noreply.github.com>
2016-07-01 16:18:08 +0530

    Initial OmniROM bringup

---
+++ omni_seedmtk.mk
@@ -0,0 +1,21 @@
+$(call inherit-product, device/google/seedmtk/seedmtk.mk)
+
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Common OmniROM stuff
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_NAME := omni_seedmtk
+PRODUCT_DEVICE :=seedmtk
+PRODUCT_BRAND := google
+PRODUCT_MANUFACTURER := Google
+PRODUCT_MODEL := Pixelv1
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="seedmtk" \
+    BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys \
+    PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
