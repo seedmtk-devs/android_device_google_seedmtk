95babc7b5581632bada884ca3b9ce29a6bcc3bc2
Harsh Shandilya <msfjarvis@gmail.com>
2016-07-12 21:23:36 -0500

    Initial PureNexus bringup
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- aosp_seedmtk.mk
+++ aosp_seedmtk.mk
@@ -1,15 +1,16 @@
-$(call inherit-product, device/google/seedmtk/seedmtk.mk)
-
-# Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit the common AOSP configurations
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := cm_seedmtk
-PRODUCT_DEVICE :=seedmtk
+PRODUCT_NAME := aosp_seedmtk
+PRODUCT_DEVICE := seedmtk
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 PRODUCT_MODEL := Pixelv1
-
+PRODUCT_MODEL := AOSP on seedmtk
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="seedmtk"
+
+$(call inherit-product, device/google/seedmtk/device.mk)
+
