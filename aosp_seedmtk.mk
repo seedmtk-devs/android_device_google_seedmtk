14917394ef30ba892b080570a49f145bcdcb0bf4
Harsh Shandilya <MSF-Jarvis@users.noreply.github.com>
2016-07-19 12:00:49 +0530

    Prep for AOSiP

--- aosp_seedmtk.mk
+++ aosp_seedmtk.mk
@@ -4,7 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := du_seedmtk
+PRODUCT_NAME := aosip_seedmtk
 PRODUCT_DEVICE :=seedmtk
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
@@ -15,4 +15,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys \
     PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-$(call inherit-product, device/google/seedmtk/seedmtk.mk)
+$(call inherit-product, device/google/seedmtk/device.mk)
