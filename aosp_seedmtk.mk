cf32cb218256ccee6b6ccc2e49613f6a742de2d0
Harsh Shandilya <MSF-Jarvis@users.noreply.github.com>
2016-07-19 12:10:38 +0530

    Remove DU vendor call

--- aosp_seedmtk.mk
+++ aosp_seedmtk.mk
@@ -1,8 +1,6 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosip_seedmtk
 PRODUCT_DEVICE :=seedmtk
