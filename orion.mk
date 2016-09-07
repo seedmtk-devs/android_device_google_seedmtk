b00479f1b33e7d0cbfbbbb1daa7f6f075a28c71b
Harsh Shandilya <msfjarvis@gmail.com>
2016-09-07 21:15:11 +0530

    Orion bringup
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- orion.mk
+++ orion.mk
@@ -1,10 +1,10 @@
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+# Inherit some common Orion stuff.
 
-PRODUCT_NAME := du_seedmtk
+$(call inherit-product, vendor/orion/config/common_full_phone.mk)
+
+PRODUCT_NAME := orion_seedmtk
 PRODUCT_DEVICE :=seedmtk
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
